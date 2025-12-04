60896672d6aec5620d93d2079e5bbcef1f547834
Francisco Kaique <fkaiquesp@gmail.com>
2025-12-04 15:11:49 -0300

    feat: footer, responsividade

---
+++ main.js
@@ -0,0 +1,8 @@
+window.addEventListener("scroll",()=>{
+    const nav = document.getElementById('nav')
+    if (window.scrollY>0){
+        nav.classList.add("scrolled")
+    }else{
+        nav.classList.remove("scrolled")
+    }
+})
